3ff2c7ed715f09ef4571d33f2bbd615e176c6c32
Ben Wu <wubenhe@gmail.com>
2015-07-31 13:18:14 -0700

    Update gridTitle.jsx.js
    add new default sorting icon

--- modules/gridTitle.jsx.js
+++ modules/gridTitle.jsx.js
@@ -46,7 +46,7 @@ var GridTitle = React.createClass({
     var nodes = this.props.columnSettings.getColumns().map(function(col,
       index) {
       var columnSort = "";
-      var sortComponent = null;
+      var sortComponent = that.props.sortSettings.sortDefaultClassName;
       var titleStyles = null;
 
       if (that.props.sortSettings.sortColumn == col && that.props.sortSettings
